1611a082ad880d5b44bc2bfe24c612d74ba82d21
deletX <37343881+deletX@users.noreply.github.com>
2020-11-04 18:10:47 +0100

    Map fix 2

--- assets/minecraft-map/baseMarkers.js
+++ assets/minecraft-map/baseMarkers.js
@@ -1,4 +1,4 @@
 overviewer.util.injectMarkerScript('/assets/minecraft-map/markersDB.js');
-overviewer.util.injectMarkerScript('/assets/minecraft-map/markersDB.js');
-overviewer.util.injectMarkerScript('/assets/minecraft-map/markersDB.js');
+overviewer.util.injectMarkerScript('/assets/minecraft-map/markers.js');
+overviewer.util.injectMarkerScript('/assets/minecraft-map/regions.js');
 overviewer.collections.haveSigns=true;
