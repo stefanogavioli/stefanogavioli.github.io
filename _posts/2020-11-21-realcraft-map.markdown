a04a83f7e1afc2b3cdb5f9743057ff953cc51fc5
deletX <37343881+deletX@users.noreply.github.com>
2020-11-21 16:05:00 +0100

    Added rlcraft map

---
+++ _posts/2020-11-21-realcraft-map.markdown
@@ -0,0 +1,62 @@
+---
+layout: post
+
+ogtype: article
+ogimage: /assets/img/minecraft-map-tb.png
+
+
+title:  "RealCraft map"
+date:   2020-11-21 15:43:00 +0200
+
+last_update: 2020-11-20 00:54:00 +0200
+categories: js leaflet minecraft
+description: Non-real-time interactive RealCraft map with leaflet
+ogdescription: Interactive RealCraft map
+excerpt_separator: <!--more-->
+---
+<script type="text/javascript" src="/assets/realcraft-map/overviewerConfig.js"></script>
+<script type="text/javascript" src="https://capodannovalsugana.ns0.it/rlcraft-map/overviewer.js"></script>
+
+<link rel="stylesheet" href="/assets/minecraft-map/leaflet.css" />
+<script src="/assets/minecraft-map/leaflet.js"></script>
+<link rel="stylesheet" href="/assets/minecraft-map/overviewer.css" type="text/css" />
+
+<small>Last update on {{ page.last_update | date: "%d %B %Y - %H:%M"}}</small>
+
+[Click here](/blog/js/leaflet/minecraft/2011/11/21/realcraft-map.html#map) to jump to the map right away!
+
+This is the same map as in the [previous article](/blog/js/leaflet/minecraft/2011/11/21/realcraft-map.html) with a different server, with RLCraft.
+<!--more-->
+
+
+{% include parallaximage.html image='/assets/img/minecraft-map.webp'%} 
+The map was made with the same process, this time though as different mod assets are not included with the standard minecraft assets it is possible to 
+see how the mod ones are missing. Yet you are able to see the outlines and the bigger picture.
+ 
+<a id="map"></a>
+Here's the map:
+<div class="mapbody" style="margin-bottom: 2rem">
+    <noscript style="color:white; background-color:black">
+        If you can see this message, there is likely a problem loading the Overviewer JavaScript components.
+        Check the JavaScript console for error messages.
+    </noscript>
+    <div id="mcmap"></div>
+    
+    <script>
+    $(document).ready(function() {
+        overviewer.util.initialize()
+        });
+    </script>
+</div>
+
+<br>
+### Data Table
+
+Data table with size of world.zip # of tiles<sup>1</sup> and map weight
+
+| Date  | world.zip | # of tiles<sup>1</sup> | map weight |
+| ------------- | ------------- | ------------- | ------------- |
+| 20/11/2020  | 129 MB  | 41 439  | 2.07 GB |
+
+<sup>1</sup>: The number of tiles includes some extra files whose count is constant: 61. Thus the exact number of tiles is the one given minus 61. 
+Also it contains all the day, night and Nether (if exists) files.
---
+++ assets/realcraft-map/overviewerConfig.js
@@ -0,0 +1,98 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "https://capodannovalsugana.ns0.it/rlcraft-map/signpost.png",
+            "signMarker": "https://capodannovalsugana.ns0.it/rlcraft-map/signpost_icon.png",
+            "bedMarker": "https://capodannovalsugana.ns0.it/rlcraft-map/bed.png",
+            "spawnMarker": "https://capodannovalsugana.ns0.it/rlcraft-map/markers/marker_home.png",
+            "spawnMarker2x": "https://capodannovalsugana.ns0.it/rlcraft-map/markers/marker_home_2x.png",
+            "queryMarker": "https://capodannovalsugana.ns0.it/rlcraft-map/markers/marker_location.png",
+            "queryMarker2x": "https://capodannovalsugana.ns0.it/rlcraft-map/markers/marker_location_2x.png",
+            "compass": {
+                "0": "https://capodannovalsugana.ns0.it/rlcraft-map/compass_upper-left.png",
+                "1": "https://capodannovalsugana.ns0.it/rlcraft-map/compass_upper-right.png",
+                "3": "https://capodannovalsugana.ns0.it/rlcraft-map/compass_lower-left.png",
+                "2": "https://capodannovalsugana.ns0.it/rlcraft-map/compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "world - overworld"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1605966216",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "Daytime",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "https://capodannovalsugana.ns0.it/rlcraft-map/day",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "world - overworld",
+            "last_rendertime": 1605916308,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -153,
+                64,
+                343
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -153,
+                64,
+                343
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Nighttime",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "https://capodannovalsugana.ns0.it/rlcraft-map/night",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "world - overworld",
+            "last_rendertime": 1605916308,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -153,
+                64,
+                343
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -153,
+                64,
+                343
+            ],
+            "north_direction": 0
+        }
+    ]
+};
